a7c278dbfa1fe6803597876aedeb109633725eb2
Marcus Vinicius <vmvini@hotmail.com>
2016-10-09 21:40:21 -0300

    tirando foto

--- www/angular-app/controllers/cameraCtrl.js
+++ www/angular-app/controllers/cameraCtrl.js
@@ -7,8 +7,34 @@
 	function cameraCtrl($scope){
 
 		var vm = this;
-
+		
 		vm.title = "Camera Page";
+		vm.picture = {};
+
+		vm.takePicture = takePicture;
+
+		function pictureTaken(imgData){
+			$scope.$apply(function(){
+				console.log("foto tirada");
+				vm.picture = imgData;
+			});
+		}
+		function fail(msg){
+			console.log("falha ao tirar foto");
+			console.log(msg);
+		}
+
+		function takePicture(){
+			navigator.camera.getPicture(pictureTaken, fail, {
+
+				quality:50, 
+				sourceType: Camera.PictureSourceType.CAMERA,
+				destinationType: Camera.DestinationType.FILE_URI
+
+			});
+		}
+
+		
 
 	}
 
